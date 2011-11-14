75a356416688bc6fa117a587e4121001d5d23b90
Sambhu Singh <sambhu@web3r.co.uk>
2011-11-14 13:05:03 +0000

    Price Entry Using Keyboard Enter Key Updates

--- production/script/FOF_Calculator.js
+++ production/script/FOF_Calculator.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){		
-    $('.price_entry').focus().select();
+    $('.price_entry').focus().select();    
     
     var rateType = 'finance';
     var resetOptions = $('#FinanceType').html(); //saves the original state of the <select> element		
@@ -64,28 +64,51 @@ $(document).ready(function(){
     }
     
     //add onKeyPress attribute on input field
-    $('.price_entry').attr('onKeyPress', 'return calculateMonInst(event)');
+    $('.price_entry').attr('onKeyPress', 'calculateMonInst(event)');    
+    $('#triggerButton').attr('onClick', 'addFocusOKButton()');
+    
 });
 
-
+  
 /**
  * When Enter key is pressed on Price Entry Input box, Calculate button is invoked!
  */
 function calculateMonInst(e){
-    if (e && e.keyCode == 13){		    
-	$('#triggerButton').click();//invoke calculate button	
-	//$('#triggerButton').blur();
+    if (e && e.keyCode == 13){
+	//If browser is not IE invoke Calculate click button
+	//In ie Calculate button is already set to focus
+	if (!$.browser.msie){
+	    $('#triggerButton').click();//invoke calculate button			  
+	}
 	
-	$('.price_entry').focus().select();		
+	//Set a focus on text box
+	    $('.price_entry').select().focus(); //works on all browser, not is IE
+
+	    /**
+	     * Unlike FireFox, IE run the focus() method before the textbox render properly. 
+	     * Because Javascript is put after textbox we need to delay the focus execute time 
+	     * using setTimeout() function.
+	     * 
+	     * http://www.mkyong.com/javascript/focus-is-not-working-in-ie-solution/
+	     */	
+	    setTimeout(function() {
+		$('.price_entry').focus();
+	    }, 10); //IE fix: focus time delay
 	
-	var boxyOKButton = $('.boxy-content .button');
-	
-	/**
-	 * If a boxy dialog box exist then set a focus on its button
-	 * Else multiple instance of boxy diolog box pop-ups when enter button clicked
-	 */
-	if (boxyOKButton){
-	    boxyOKButton.focus();
-	} 
+	addFocusOKButton();	
     }
+}
+
+/**
+ * Adding foucs on OK Button, so when Enter is presed on keyboard
+ * another pop-up dialog doesn't appear while Boxy Dialog box is open
+ */
+function addFocusOKButton(){ //set a focus on ok button    
+    var boxyOKButton = $('.boxy-content .button');
+    if (boxyOKButton){	
+	boxyOKButton.focus();//firefox
+	setTimeout(function() {
+	    boxyOKButton.focus();
+	}, 10); //IE fix 	
+    }    
 }
--- production/script/IHS_Calculator.js
+++ production/script/IHS_Calculator.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){		
+$(document).ready(function(){	
+    $('.price_entry').focus().select();  
+    
     var rateType = 'ideal';
     var resetOptions = $('#FinanceType').html(); //saves the original state of the <select> element
 		
@@ -58,4 +60,52 @@ $(document).ready(function(){
 	}
     }
 
+    //add onKeyPress attribute on input field
+    $('.price_entry').attr('onKeyPress', 'calculateMonInst(event)');    
+    $('#triggerButton').attr('onClick', 'addFocusOKButton()');
+    
 });
+
+  
+/**
+ * When Enter key is pressed on Price Entry Input box, Calculate button is invoked!
+ */
+function calculateMonInst(e){
+    if (e && e.keyCode == 13){
+	//If browser is not IE invoke Calculate click button
+	//In ie Calculate button is already set to focus
+	if (!$.browser.msie){
+	    $('#triggerButton').click();//invoke calculate button			  
+	}
+	
+	//Set a focus on text box
+	    $('.price_entry').select().focus(); //works on all browser, not is IE
+
+	    /**
+	     * Unlike FireFox, IE run the focus() method before the textbox render properly. 
+	     * Because Javascript is put after textbox we need to delay the focus execute time 
+	     * using setTimeout() function.
+	     * 
+	     * http://www.mkyong.com/javascript/focus-is-not-working-in-ie-solution/
+	     */	
+	    setTimeout(function() {
+		$('.price_entry').focus();
+	    }, 10); //IE fix: focus time delay
+	
+	addFocusOKButton();	
+    }
+}
+
+/**
+ * Adding foucs on OK Button, so when Enter is presed on keyboard
+ * another pop-up dialog doesn't appear while Boxy Dialog box is open
+ */
+function addFocusOKButton(){ //set a focus on ok button    
+    var boxyOKButton = $('.boxy-content .button');
+    if (boxyOKButton){	
+	boxyOKButton.focus();//firefox
+	setTimeout(function() {
+	    boxyOKButton.focus();
+	}, 10); //IE fix 	
+    }    
+}
