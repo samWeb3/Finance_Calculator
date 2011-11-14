95e41b021f03c2aea4cb6c2ed7340fd137b7985c
Sambhu Singh <sambhu@web3r.co.uk>
2011-11-14 15:04:05 +0000

    Files and Folder Rearranged

--- production/templating/FOF_Widget_PreLoad.js
+++ production/templating/FOF_Widget_PreLoad.js
@@ -1,10 +1,1 @@
-var FinanceWidget = {
-    financeOption: 'Select Finance Option:', 
-    financeDeposit: 'Select Deposit:'		
-};
-
-//Loading External Template
-$.get('production/templating/FinanceCalculator_Widget.html', function(fc){
-    $.tmpl(fc, FinanceWidget).appendTo("#Finance_Widget");
-});
-  
+var FinanceWidget={financeOption:"Select Finance Option:",financeDeposit:"Select Deposit:"};$.get("production/templating/FinanceCalculator_Widget.html",function(a){$.tmpl(a,FinanceWidget).appendTo("#Finance_Widget")});
